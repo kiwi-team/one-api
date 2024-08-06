1540c920edbaed94b593c8200f7b2ab3e265b6d6
wm <mingazi@163.com>
2024-08-06 17:41:41 +0800

    baichuan4

--- relay/adaptor/baichuan/constants.go
+++ relay/adaptor/baichuan/constants.go
@@ -1,6 +1,9 @@
 package baichuan
 
 var ModelList = []string{
+	"Baichuan4",
+	"Baichuan3-Turbo",
+	"Baichuan3-Turbo-128k",
 	"Baichuan2-Turbo",
 	"Baichuan2-Turbo-192k",
 	"Baichuan-Text-Embedding",
