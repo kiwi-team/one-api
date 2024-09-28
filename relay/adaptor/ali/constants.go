d350b8d48347979ae990d999992973a35e0b2d27
wm <mingazi@163.com>
2024-09-29 00:43:36 +0800

    qwen2.5-72b-instruct

--- relay/adaptor/ali/constants.go
+++ relay/adaptor/ali/constants.go
@@ -2,6 +2,7 @@ package ali
 
 var ModelList = []string{
 	"qwen-turbo", "qwen-plus", "qwen-max", "qwen-max-longcontext",
+	"qwen2.5-72b-instruct", "qwen2.5-32b-instruct", "qwen2.5-14b-instruct", "qwen2.5-7b-instruct",
 	"text-embedding-v1",
 	"ali-stable-diffusion-xl", "ali-stable-diffusion-v1.5", "wanx-v1",
 }
---
+++ relay/adaptor/friday/constants.go
@@ -0,0 +1,8 @@
+package friday
+
+// https://console.volcengine.com/ark/region:ark+cn-beijing/model
+
+var ModelList = []string{
+	"LongCat-Prime-8K-Chat",
+	"LongCat-Large-32K-Chat",
+}
