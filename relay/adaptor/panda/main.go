bd72343c94f8a661caa653eb724f7d19287fd39b
wm <mingazi@163.com>
2024-11-03 22:34:10 +0800

    panda

---
+++ relay/adaptor/panda/main.go
@@ -0,0 +1,19 @@
+package panda
+
+import (
+	"fmt"
+
+	"github.com/songquanpeng/one-api/relay/meta"
+	"github.com/songquanpeng/one-api/relay/relaymode"
+)
+
+func GetRequestURL(meta *meta.Meta) (string, error) {
+	switch meta.Mode {
+	case relaymode.ChatCompletions:
+		return fmt.Sprintf("%s/v1/openai/native/chat/completions", meta.BaseURL), nil
+	case relaymode.Embeddings:
+		return fmt.Sprintf("%s/v1/openai/native/embeddings", meta.BaseURL), nil
+	default:
+	}
+	return "", fmt.Errorf("unsupported relay mode %d for doubao", meta.Mode)
+}
