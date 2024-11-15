595a101f01f5093557d039a676abb2700221ff0d
wm <mingazi@163.com>
2024-11-16 07:44:11 +0800

    friday add gpt-4o-mini dall-e-3

--- relay/adaptor/friday/main.go
+++ relay/adaptor/friday/main.go
@@ -11,6 +11,8 @@ func GetRequestURL(meta *meta.Meta) (string, error) {
 	switch meta.Mode {
 	case relaymode.ChatCompletions:
 		return fmt.Sprintf("%s/v1/openai/native/chat/completions", meta.BaseURL), nil
+	case relaymode.ImagesGenerations:
+		return fmt.Sprintf("%s/v1/openai/native/images/generations", meta.BaseURL), nil
 	case relaymode.Embeddings:
 		return fmt.Sprintf("%s/v1/openai/native/embeddings", meta.BaseURL), nil
 	default:
