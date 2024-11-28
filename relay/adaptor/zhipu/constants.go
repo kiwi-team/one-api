87a25cf789c2e33b61e2b7929723523deaa8a0fc
wm <mingazi@163.com>
2024-11-28 15:47:33 +0800

    glm-4-plus

--- relay/adaptor/zhipu/constants.go
+++ relay/adaptor/zhipu/constants.go
@@ -1,6 +1,7 @@
 package zhipu
 
 var ModelList = []string{
+	"glm-4-plus",
 	"chatglm_turbo", "chatglm_pro", "chatglm_std", "chatglm_lite",
 	"glm-4", "glm-4v", "glm-3-turbo", "embedding-2",
 	"cogview-3", "glm-4-flash",
