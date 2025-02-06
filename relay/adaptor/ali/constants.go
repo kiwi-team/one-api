0478a01f02c20b2c13418ca34a6fbe95a886ccf8
wm <mingazi@163.com>
2025-02-06 11:31:44 +0800

    qwen-max-lastest

--- relay/adaptor/ali/constants.go
+++ relay/adaptor/ali/constants.go
@@ -1,6 +1,8 @@
 package ali
 
 var ModelList = []string{
+	"qwen-max-2025-01-25",
+	"qwen-max-latest",
 	"qwen-turbo", "qwen-plus", "qwen-max", "qwen-max-longcontext",
 	"qwen2.5-72b-instruct", "qwen2.5-32b-instruct", "qwen2.5-14b-instruct", "qwen2.5-7b-instruct",
 	"text-embedding-v1",
