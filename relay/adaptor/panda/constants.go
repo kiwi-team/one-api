39eba9c7bfb41edcfed4cf6480b33d9038e93bef
wm <mingazi@163.com>
2025-04-30 14:57:59 +0800

    panda 支持 gemini-2.5-pro-exp-03-25

--- relay/adaptor/panda/constants.go
+++ relay/adaptor/panda/constants.go
@@ -106,5 +106,6 @@ var ModelList = []string{
 	"gemini-2.0-flash-thinking-exp-1219",
 	"gemini-2.0-pro-exp-02-05",
 	"gemini-2.5-pro-preview-03-25",
+	"gemini-2.5-pro-exp-03-25",
 	"text-moderation-stable",
 }
