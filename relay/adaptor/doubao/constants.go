589dc6af44ee559479474cecedccd4dbfa469fba
wm <mingazi@163.com>
2025-05-13 13:17:21 +0800

    doubao-1-5-thinking-version-pro-250428

--- relay/adaptor/doubao/constants.go
+++ relay/adaptor/doubao/constants.go
@@ -8,6 +8,7 @@ var ModelList = []string{
 	"doubao-1-5-vision-lite-250315",
 	"doubao-1-5-vision-pro-250328",
 	"doubao-1-5-vision-pro-32k-250115",
+	"doubao-1-5-thinking-vision-pro-250428",
 	"doubao-1-5-thinking-pro-250415",
 	"doubao-1-5-ui-tars-250328",
 	"doubao-1-5-pro-32k-250115",
