bc6846e7bf352c6b22aa210f9d758b0126508c6e
wm <mingazi@163.com>
2025-06-13 14:13:01 +0800

    chataiapi gemini-2.5-flash-preview-05-20

--- relay/adaptor/chataiapi/constants.go
+++ relay/adaptor/chataiapi/constants.go
@@ -4,6 +4,8 @@ var ModelList = []string{
 	"gpt-4-vision-preview",
 	"claude-opus-4-20250514",
 	"claude-sonnet-4-20250514",
+	"gemini-2.5-flash-preview-04-17",
+	"gemini-2.5-flash-preview-05-20",
 	"gemini-2.5-pro-preview-05-06",
 	"gemini-2.5-pro-exp-03-25",
 	"gemini-2.5-pro-preview-03-25",
